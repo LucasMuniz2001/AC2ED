50c81dfd35d0035be587621a598007db8efdcdfc
LucasMuniz2001 <110582152+LucasMuniz2001@users.noreply.github.com>
2024-03-03 23:31:42 -0300

    Update geometria.go

--- geometria.go
+++ geometria.go
@@ -1,1 +1,11 @@
+package geometria
 
+// Area calcula a área de um retângulo
+func Area(base, altura float64) float64 {
+    return base * altura
+}
+
+// Perimetro calcula o perímetro de um retângulo
+func Perimetro(base, altura float64) float64 {
+    return 2 * (base + altura)
+}
